70da0b561d7e1db1ac868c2104934326e3ce4936
Brandon Batt <brandon.batt.ext@bayer.com>
2024-06-19 18:06:32 -0400

    Bump cmake version

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ source .env
 
 if [[ ${BUILD_TIMESCALE} =~ [Ff][Aa][Ll][Ss][Ee] ]];then
   if [[ $(dpkg -l | grep "docker-compose") > /dev/null ]];then
-    docker-compose -f docker-compose.build.yml build postgis-prod -t $TAG
+    docker-compose -f docker-compose.build.yml build postgis-prod
   else
-    docker compose -f docker-compose.build.yml build postgis-prod -t $TAG
+    docker compose -f docker-compose.build.yml build postgis-prod
   fi
 
 else
   if [[ $(dpkg -l | grep "docker-compose") > /dev/null ]];then
-  docker-compose -f docker-compose.build-timescale.yml build postgis-prod -t $TAG
+  docker-compose -f docker-compose.build-timescale.yml build postgis-prod
   else
-  docker compose -f docker-compose.build-timescale.yml build postgis-prod -t $TAG
+  docker compose -f docker-compose.build-timescale.yml build postgis-prod
   fi
 fi
